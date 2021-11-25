eb4fd90913b9ff8c859c8955f26b690047643125
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-11-25 16:21:21 +0100

    inizio corretto

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const studente = {
-    "nome":[lorenzo, luca],
-    "cognome":[rossi, verdi],
-    "eta":[18, 19, 20, 21, 22]
-}
+    nome:['lorenzo', 'luca', 'sara'],
+    cognome:['rossi', 'verdi'],
+    eta:[18, 19, 20, 21, 22]
+};
 console.log(studente);
