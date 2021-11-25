f9de2c6083d8ebb3e474427711146be8738a97fd
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-11-25 17:03:08 +0100

    penultimo es

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
 const studente = {
-    nome:['lorenzo', 'luca', 'sara'],
-    cognome:['rossi', 'verdi'],
-    eta:[18, 19, 20, 21, 22]
+    nome:'lorenzo',
+    cognome:'rossi',
+    eta: 21,
 };
-console.log(studente);
+
+for(let key in studente) {
+    // console.log(studente[key])
+}
+
+const studenti = [
+    {
+        nome:'lorenzo',
+        cognome:'rossi',
+        eta: 21,
+    },
+    {
+        nome:'francesco',
+        cognome:'verdi',
+        eta: 20,
+    },
+    {
+        nome:'sara',
+        cognome:'bianchi',
+        eta: 19,
+    }
+]
+for(let i = 0; i < studenti.length; i++) {
+    // console.log(studenti[i]);
+    const studente = studenti[i];
+    console.log(studente['nome'],studente['cognome'])
+}
+
